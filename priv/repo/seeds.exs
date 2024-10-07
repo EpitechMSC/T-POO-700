a9375f6e7a450d320accc05378086fda88cf0c26
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2024-10-07 23:50:26 +0200

    add seed for User test

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,5 +7,51 @@
 #
 #     TimeManager.Repo.insert!(%TimeManager.SomeSchema{})
 #
+# TimeManager.TimeTracker.WorkinTime
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias TimeManager.Repo
+alias TimeManager.TimeTracker.User
+alias TimeManager.TimeTracker.Clock
+alias TimeManager.TimeTracker.WorkinTime
+
+#delete all records
+
+# Repo.delete_all(User)
+# Repo.delete_all(Clock)
+# Repo.delete_all(WorkinTime)
+
+#insert User
+
+Repo.insert!(%User{
+  email: "test@test.com",
+  username: "test"
+})
+
+Repo.insert!(%User{
+  email: "test_2@test.fr",
+  username: "test2"
+})
+
+#insert WorkinTime
+
+#Repo.insert!(%WorkinTime{
+#  user_id: 1,
+#  start: "2021-09-01 08:00:00",
+#  end: "2021-09-01 12:00:00"
+#})
+#
+#Repo.insert!(%WorkinTime{
+#  user_id: 1,
+#  start: "2021-09-01 13:00:00",
+#  end: "2021-09-01 17:00:00"
+#})
+#
+##insert clock
+#
+#Repo.insert!(%Clock{
+#  user_id: 1,
+#  time: DateTime.utc_now(),
+#  status: 0
+#})
