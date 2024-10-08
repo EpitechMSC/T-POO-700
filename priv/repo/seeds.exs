18ffa98d4b92c02a84678a69fd1df9c40808c85d
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2024-10-08 12:30:24 +0200

    feat/seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,9 +11,9 @@
 # and so on) as they will fail if something goes wrong.
 
 alias TimeManager.Repo
-alias TimeManager.TimeTracker.User
-alias TimeManager.TimeTracker.Clock
-alias TimeManager.TimeTracker.WorkinTime
+alias TimeManager.Accounts.User
+alias TimeManager.Clocks.Clock
+alias TimeManager.Work.WorkingTime
 
 #delete all records
 
@@ -31,4 +31,26 @@ Repo.insert!(%User{
 Repo.insert!(%User{
   email: "test_2@test.fr",
   username: "test2"
+})
+
+#insert Clock
+
+Repo.insert!(%Clock{
+  time: "2024-09-01 08:00:00",
+  status: 1,
+  user: 1
+})
+
+Repo.insert!(%Clock{
+  time: "2024-09-01 12:00:00",
+  status: 0,
+  user: 1
+})
+
+#insert WorkinTime
+
+Repo.insert!(%WorkingTime{
+  start: "2024-09-03 08:00:00",
+  end: "2024-09-03 12:00:00",
+  user: 1
 })
