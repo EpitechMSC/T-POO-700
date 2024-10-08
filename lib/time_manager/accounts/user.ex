4f3c013d96129f41618f600bf8f9d042e0e6dabe
Axel Martin <martin.axel@sfr.fr>
2024-10-08 11:10:19 +0200

    Create uniquess migration for username and email, add email validation with regex in user creation

--- lib/time_manager/accounts/user.ex
+++ lib/time_manager/accounts/user.ex
@@ -12,7 +12,10 @@ defmodule TimeManager.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :email])
-    |> validate_required([:username, :email])
+    |> cast(attrs, [:email, :username])
+    |> validate_required([:email, :username])
+    |> validate_format(:email, ~r/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "is invalid")
+    |> unique_constraint(:email, message: "Email has already been taken")
+    |> unique_constraint(:username, message: "Username has already been taken")
   end
 end
