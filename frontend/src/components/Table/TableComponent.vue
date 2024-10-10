d416fb3629b02851d4d334a4bb6642710ac534a3
Axel Martin <martin.axel@sfr.fr>
2024-10-10 09:53:07 +0200

    Create reuasable table component, add it into home page for testing purpose

---
+++ frontend/src/components/Table/TableComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="overflow-x-auto">
+    <table class="min-w-full table-auto border-collapse">
+      <thead class="bg-gray-100">
+        <tr class="border-b border-gray-200">
+          <th
+            v-for="(header, index) in headers"
+            :key="index"
+            class="px-4 py-2 text-left text-sm font-semibold text-gray-600"
+          >
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody class="text-gray-600 text-sm">
+        <tr
+          v-for="(item, rowIndex) in data"
+          :key="rowIndex"
+          class="border-b border-gray-200"
+        >
+          <td
+            v-for="(header, colIndex) in headers"
+            :key="colIndex"
+            class="px-4 py-3"
+          >
+            <slot name="customCell" :item="item" :field="header">
+              {{ item[header] }}
+            </slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableComponent',
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    headers() {
+      return this.data.length ? Object.keys(this.data[0]) : [];
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- frontend/src/pages/Home/HomePage.ts
+++ frontend/src/pages/Home/HomePage.ts
@@ -1,3 +1,19 @@
-export default {
-  name: 'HomePageComponent',
-};
+import { defineComponent } from 'vue';
+import { User } from '../../app/models/user';
+import TableComponent from '../../components/Table/TableComponent.vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    TableComponent,
+  },
+  data() {
+    return {
+      users: [
+        { id: 1, username: 'john_doe', email: 'john@example.com' },
+        { id: 2, username: 'jane_doe', email: 'jane@example.com' },
+        { id: 3, username: 'sam_smith', email: 'sam@example.com' },
+      ] as User[],
+    };
+  },
+});
