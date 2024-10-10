cd944fac8af4d546492d7f234e47f7e872fdfff2
Axel Martin <martin.axel@sfr.fr>
2024-10-10 13:37:36 +0200

    Update controller and schemas to return json

--- backend/lib/time_manager_web/controllers/clock_controller.ex
+++ backend/lib/time_manager_web/controllers/clock_controller.ex
@@ -8,28 +8,28 @@ defmodule TimeManagerWeb.ClockController do
 
   def index(conn, _params) do
     clocks = Clocks.list_clocks()
-    render(conn, :index, clocks: clocks)
+    json(conn, clocks)
   end
 
   def create(conn, %{"clock" => clock_params}) do
     with {:ok, %Clock{} = clock} <- Clocks.create_clock(clock_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/clocks/#{clock}")
-      |> render(:show, clock: clock)
+      |> put_resp_header("location", ~p"/api/clocks/#{clock.id}")
+      |> json(clock)
     end
   end
 
   def show(conn, %{"id" => id}) do
     clock = Clocks.get_clock!(id)
-    render(conn, :show, clock: clock)
+    json(conn, clock)
   end
 
   def update(conn, %{"id" => id, "clock" => clock_params}) do
     clock = Clocks.get_clock!(id)
 
-    with {:ok, %Clock{} = clock} <- Clocks.update_clock(clock, clock_params) do
-      render(conn, :show, clock: clock)
+    with {:ok, %Clock{} = updated_clock} <- Clocks.update_clock(clock, clock_params) do
+      json(conn, updated_clock)
     end
   end
 
--- backend/lib/time_manager_web/controllers/working_time_controller.ex
+++ backend/lib/time_manager_web/controllers/working_time_controller.ex
@@ -8,28 +8,28 @@ defmodule TimeManagerWeb.WorkingTimeController do
 
   def index(conn, _params) do
     workingtimes = Work.list_workingtimes()
-    render(conn, :index, workingtimes: workingtimes)
+    json(conn, workingtimes)
   end
 
   def create(conn, %{"working_time" => working_time_params}) do
     with {:ok, %WorkingTime{} = working_time} <- Work.create_working_time(working_time_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/workingtimes/#{working_time}")
-      |> render(:show, working_time: working_time)
+      |> put_resp_header("location", ~p"/api/workingtimes/#{working_time.id}")
+      |> json(working_time)
     end
   end
 
   def show(conn, %{"id" => id}) do
     working_time = Work.get_working_time!(id)
-    render(conn, :show, working_time: working_time)
+    json(conn, working_time)
   end
 
   def update(conn, %{"id" => id, "working_time" => working_time_params}) do
     working_time = Work.get_working_time!(id)
 
-    with {:ok, %WorkingTime{} = working_time} <- Work.update_working_time(working_time, working_time_params) do
-      render(conn, :show, working_time: working_time)
+    with {:ok, %WorkingTime{} = updated_working_time} <- Work.update_working_time(working_time, working_time_params) do
+      json(conn, updated_working_time)
     end
   end
 
@@ -40,22 +40,23 @@ defmodule TimeManagerWeb.WorkingTimeController do
       send_resp(conn, :no_content, "")
     end
   end
+
   def search_by_userid_and_date_range(conn, %{"id" => id, "start" => start_date_as_string} = params) do
     end_date =
       case Map.get(params, "end") do
-        nil -> nil # `end` is not provided, set it to nil
+        nil -> nil
         end_date_as_string ->
-          # Parse the `end` date and handle both success and failure cases
           case NaiveDateTime.from_iso8601(end_date_as_string) do
-            {:ok, parsed_date} -> parsed_date # If parsing succeeds, return the parsed date
-            {:error, _reason} -> nil          # If parsing fails, return nil
+            {:ok, parsed_date} -> parsed_date
+            {:error, _reason} -> nil
           end
-        end
+      end
+
     {:ok, start_date} = NaiveDateTime.from_iso8601(start_date_as_string)
 
-    case Work.find_working_time_for_user_and_date_range(id,start_date,end_date) do
+    case Work.find_working_time_for_user_and_date_range(id, start_date, end_date) do
       {:ok, works} ->
-        render(conn, :index, workingtimes: works)
+        json(conn, works)
 
       {:error, :not_found} ->
         conn
