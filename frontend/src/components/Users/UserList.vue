5b77d35a9cd4d9bed32a65d4e67b90ff9dad7aea
Axel Martin <martin.axel@sfr.fr>
2024-10-10 11:28:53 +0200

    Add error handling in user list

--- frontend/src/components/Users/UserList.vue
+++ frontend/src/components/Users/UserList.vue
@@ -1,5 +1,20 @@
 <template>
-  <TableComponent v-if="!loading && !error" :data="users" />
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Liste des utilisateurs</h1>
+
+    <div v-if="loading" class="text-gray-500">Chargement...</div>
+    <div v-if="error" class="text-red-600">{{ error }}</div>
+
+    <div v-if="!loading && !error">
+      <TableComponent v-if="users.length > 0" :data="users" />
+      <div
+        v-else
+        class="text-center text-gray-500 border border-gray-300 rounded p-4"
+      >
+        <p class="text-lg">Aucun utilisateur trouvé.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
