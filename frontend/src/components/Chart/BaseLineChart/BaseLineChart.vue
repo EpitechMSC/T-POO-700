e456f23efc82a88858a64d3c2c7d78a131c3c68e
Axel Martin <martin.axel@sfr.fr>
2024-10-14 21:07:38 +0200

    fix base input, create base line chart for charts

---
+++ frontend/src/components/Chart/BaseLineChart/BaseLineChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="base-line-chart">
+    <Line :data="data" :options="options" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Line } from 'vue-chartjs';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  Filler,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+import { BaseLineChartProps } from './BaseLineChart.types';
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  Filler,
+  CategoryScale,
+  LinearScale
+);
+
+export default defineComponent({
+  name: 'BaseLineChart',
+  components: {
+    Line,
+  },
+  props: {
+    data: {
+      type: Object as PropType<BaseLineChartProps['data']>,
+      required: true,
+    },
+    options: {
+      type: Object as PropType<BaseLineChartProps['options']>,
+      default: () => ({}),
+    },
+  },
+});
+</script>
+
+<style scoped>
+.base-line-chart {
+  width: 100%;
+}
+</style>
--- frontend/src/components/Inputs/BaseInput/BaseInput.vue
+++ frontend/src/components/Inputs/BaseInput/BaseInput.vue
@@ -1,3 +1,4 @@
+<!-- src/components/BaseInput/BaseInput.vue -->
 <template>
   <div class="input-wrapper">
     <label
@@ -12,7 +13,7 @@
       :id="id"
       :name="name"
       :placeholder="placeholder"
-      v-model="modelValue"
+      v-model="internalValue"
       :class="[
         'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-gray-600 focus:border-gray-600 block w-full p-2.5',
         darkModeClasses,
@@ -32,15 +33,25 @@ export default defineComponent({
   name: 'BaseInput',
   props: BaseInputProps,
   emits: ['update:modelValue'],
-  setup(props, {}) {
+  setup(props, { emit }) {
     const darkModeClasses = computed(() =>
       props.darkMode
         ? 'dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         : ''
     );
 
+    const internalValue = computed({
+      get() {
+        return props.modelValue;
+      },
+      set(value: string) {
+        emit('update:modelValue', value);
+      },
+    });
+
     return {
       darkModeClasses,
+      internalValue,
     };
   },
 });
