1ec550157fbf5817f5e70c5f9c5ebe6dd3fe13b6
Jean Clémente Radigois <jeanclementeradigois@gmail.com>
2024-10-14 09:37:28 +0200

    Feat : added functionality for clocking

--- frontend/src/components/TopBar/TopBar.vue
+++ frontend/src/components/TopBar/TopBar.vue
@@ -6,10 +6,12 @@
       Bonjour, Gilbert
     </h1>
     <div class="clock flex items-center">
-      <p class="text-xl mr-2 w-20">
-        {{ time }}
-      </p>
+      <span class="flex">
+        <p v-if="!isClocked" class="text-xl mr-1">{{ timeOfClockedIn }} -</p>
+        <p class="text-xl mr-2 w-20">{{ time }}</p>
+      </span>
       <button
+        @click="clocking"
         class="bg-white p-2 rounded-lg w-14 h-14 flex justify-center items-center"
       >
         <svg
@@ -22,7 +24,7 @@
           <g clip-path="url(#clip0_15_254)">
             <path
               d="M17 8.50001V17L22.6666 19.8333M31.1666 17C31.1666 24.824 24.824 31.1667 17 31.1667C9.17595 31.1667 2.83331 24.824 2.83331 17C2.83331 9.17598 9.17595 2.83334 17 2.83334C24.824 2.83334 31.1666 9.17598 31.1666 17Z"
-              stroke="#1E1E1E"
+              :stroke="isClocked ? 'black' : 'red'"
               stroke-width="4"
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -45,36 +47,104 @@
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue';
-import { useWorkingTimesStore } from '../../app/store/store';
+import { useWorkingTimesStore, useClocksStore } from '../../app/store/store';
+import { useToast } from 'vue-toastification';
 
 export default defineComponent({
   name: 'TopBar',
   setup() {
+    const toast = useToast();
+
     const workingTimesStore = useWorkingTimesStore();
+    const clockStore = useClocksStore();
 
     const time = ref('');
-    var workedToday = ref('');
+    const date = ref('');
+    const dateOfClock = ref({ start: '', end: '' });
+    const isClocked = ref(true);
+    const timeOfClockedIn = ref('');
+    let workedToday = ref('');
 
     const updateTime = () => {
       const now = new Date();
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
-      const seconds = now.getSeconds();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
+      const day = String(now.getDate()).padStart(2, '0');
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      const seconds = String(now.getSeconds()).padStart(2, '0');
 
       time.value = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+      date.value = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+    };
+    const triggerToast = () => {
+      this.toast('Hi from LogRocket', {
+        position: 'top-right',
+        timeout: 5000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: 'button',
+        icon: 'fas fa-rocket',
+        rtl: false,
+      });
+    };
+    const clocking = async () => {
+      const dataToSend = {
+        clock: {
+          time: date.value,
+          status: isClocked.value,
+        },
+      };
+      try {
+        await clockStore.createClock(dataToSend);
+      } catch (error) {
+        console.error('There was an error during clocking', error);
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      isClocked.value
+        ? ((dateOfClock.value.start = date.value),
+          (timeOfClockedIn.value = time.value),
+          (dateOfClock.value.end = ''))
+        : (dateOfClock.value.end = date.value);
+
+      isClocked.value = !isClocked.value;
+
+      if (dateOfClock.value.end != '') {
+        const workingTimeDataToSend = {
+          working_time: {
+            start: dateOfClock.value.start,
+            end: dateOfClock.value.end,
+            user: 1,
+          },
+        };
+        try {
+          await workingTimesStore.createWorkingTime(workingTimeDataToSend);
+          triggerToast();
+        } catch (error) {
+          console.error(
+            'There was an error when creating a working time',
+            error
+          );
+        }
+      }
     };
 
     onMounted(() => {
       workingTimesStore.getWorkingTimeById(1);
-
-      console.log(workingTimesStore);
-
       updateTime();
       setInterval(updateTime, 1000);
     });
     return {
       time,
       workedToday,
+      clocking,
+      isClocked,
+      timeOfClockedIn,
     };
   },
 });
