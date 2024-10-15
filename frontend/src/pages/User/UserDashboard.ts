3094f27577dfbac2df05e5393c3913888156c8bc
Axel Martin <martin.axel@sfr.fr>
2024-10-15 16:03:24 +0200

    Separate Working time chart into multiple files, update path in user dashboard

--- frontend/src/pages/User/UserDashboard.ts
+++ frontend/src/pages/User/UserDashboard.ts
@@ -1,7 +1,7 @@
 import { defineComponent } from 'vue';
 import NavBar from '../../components/NavBar/NavBar.vue';
 import SummaryCard from '../../components/Card/SummaryCard/SummaryCard.vue';
-import WorkingTimeChart from '../../components/Chart/WorkingTimeChart.vue';
+import WorkingTimeChart from '../../components/Chart/WorkingTimeChart/WorkingTimeChart.vue';
 import WorkingTimeList from '../../components/WorkingTimes/WorkingTimeList.vue';
 import TopBar from '../../components/TopBar/TopBar.vue';
 
