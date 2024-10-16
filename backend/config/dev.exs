34b8193cfb0b28f1d41b941a557f34897a3e3617
Axel Martin <martin.axel@sfr.fr>
2024-10-16 16:51:54 +0200

    remove all unecessary dependancies from backend

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -23,11 +23,8 @@ config :time_manager, TimeManagerWeb.Endpoint,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: "jz9lJjC802eqy6Et0WhX0Ne/yDntpXtvffMz93mbsEcR2xfo/A3MihFYyCOhCNzI",
-  watchers: [
-    esbuild: {Esbuild, :install_and_run, [:time_manager, ~w(--sourcemap=inline --watch)]},
-    tailwind: {Tailwind, :install_and_run, [:time_manager, ~w(--watch)]}
-  ]
+  secret_key_base: "jz9lJjC802eqy6Et0WhX0Ne/yDntpXtvffMz93mbsEcR2xfo/A3MihFYyCOhCNzI"
+
 
 # ## SSL Support
 #
