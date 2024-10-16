552484f5f313cfea02b211d8f3a842acbdb25e6a
Jean Clémente Radigois <jeanclementeradigois@gmail.com>
2024-10-16 10:05:20 +0200

    feat: added batsignal on the front with a banderole for all users

---
+++ frontend/src/app/store/modules/signal.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia';
+import router from '../../router/router';
+import { SignalModel } from '../../models/signal';
+import SignalMethods from '../../api/modules/signal';
+
+export const useSignalStore = defineStore('signal', {
+  state: (): SignalModel => ({
+    name: 'BatSignal',
+    status: false,
+  }),
+
+  getters: {
+    getUser: (state): SignalModel | null => state,
+  },
+
+  actions: {
+    async getStatus(): Promise<boolean> {
+      try {
+        const response = await SignalMethods.getSignalStatus();
+        const data = response.data;
+        return data.status;
+      } catch (error) {
+        console.error('Login failed:', error);
+        throw new Error('Échec de la connexion');
+      }
+    },
+    async toggleStatus(): Promise<void> {
+        try {
+          const response = await SignalMethods.getSignalStatus();
+          const data = response.data;
+
+          await SignalMethods.updateSignalStatus(!data.status)
+          
+        } catch (error) {
+          console.error('Login failed:', error);
+          throw new Error('Échec de la connexion');
+        }
+      },
+  },
+});
