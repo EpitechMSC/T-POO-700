1c2c51e12e276ff21c675e705c9a3ab5e0295272
Jean Clémente Radigois <jeanclementeradigois@gmail.com>
2024-10-17 10:57:26 +0200

    Fix: error with Bat SIgnal

--- frontend/src/app/api/modules/signal.ts
+++ frontend/src/app/api/modules/signal.ts
@@ -4,11 +4,14 @@ import { requests } from '../config/axiosConfig';
 
 const SignalMethods = {
   getSignalStatus: (): Promise<SignalModel> =>
-    requests.get<SignalModel>('signal/1'),
-  updateSignalStatus: (status: boolean) =>
-    requests.put<SignalModel>(`signal/1`, {signal:{
-      name: 'BatSignal',
-      status: status,
-    }}),
+    requests.get<SignalModel>('signal'),
+  updateSignalStatus: (data: SignalModel) =>
+    requests.put<SignalModel>(`signal/` + data.id, {
+      signal: {
+        name: 'BatSignal',
+        status: !data.status,
+      },
+    }),
 };
 export default SignalMethods;
+  
