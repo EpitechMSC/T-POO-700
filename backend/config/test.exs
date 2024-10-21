d3421940329dbdf559993403990da6eb0bcc14c5
Axel Martin <martin.axel@sfr.fr>
2024-10-21 15:12:50 +0200

    update

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -15,7 +15,6 @@ config :time_manager, TimeManager.Repo,
 
 config :time_manager, :env, :test
 
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :time_manager, TimeManagerWeb.Endpoint,
