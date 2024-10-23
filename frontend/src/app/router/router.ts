7ab241760cf7984275f5516b27a4f205a7d0b938
Axel Martin <martin.axel@sfr.fr>
2024-10-23 16:49:00 +0200

    Add roles in frontend

--- frontend/src/app/router/router.ts
+++ frontend/src/app/router/router.ts
@@ -1,20 +1,9 @@
-import {
-  RouteRecordRaw,
-  createRouter,
-  createWebHistory,
-  NavigationGuard,
-  RouteLocationNormalized,
-  NavigationGuardNext,
-} from 'vue-router';
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 import UserDashboard from '../../pages/User/UserDashboard.vue';
-import { useAuthenticateStore } from '../store/store';
 import SettingsPage from '../../pages/Settings/SettingsPage.vue';
 import WorkingTimesPage from '../../pages/WorkingTimes/WorkingTimesPage.vue';
 import Calendar from '../../pages/Calendar/Calendar.vue';
-
-interface RouteMeta {
-  requiresAuth?: boolean;
-}
+import beforeEachGuard from './auth-guard';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', redirect: '/login' },
@@ -40,7 +29,7 @@ const routes: Array<RouteRecordRaw> = [
     path: '/users/:id/settings',
     name: 'Settings',
     component: SettingsPage,
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true }, //, roles: ['Supervisor', 'Manager']
   },
   {
     path: '/login',
@@ -58,39 +47,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
-
-const beforeEachGuard: NavigationGuard = (
-  to: RouteLocationNormalized,
-  _from: RouteLocationNormalized,
-  next: NavigationGuardNext
-) => {
-  const authenticateStore = useAuthenticateStore();
-
-  const user = authenticateStore.getUser;
-
-  if (
-    authenticateStore.isAuthenticated &&
-    (to.name === 'Login' || to.name === 'Register')
-  ) {
-    if (user) {
-      next(`/users/${user.id}`);
-    } else {
-      next('/login');
-    }
-  } else if (
-    to.matched.some(
-      (record: { meta: RouteMeta }) => (record.meta as RouteMeta).requiresAuth
-    )
-  ) {
-    if (!authenticateStore.isAuthenticated) {
-      next('/login');
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
-};
 
 router.beforeEach(beforeEachGuard);
 
