dac49f03d4e53596f88b168a23450c8f16d4ccb1
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2024-10-24 02:25:09 +0200

    Updated settings pages and added a controller for document downloads. Downloads still not functioning.

--- frontend/src/pages/Settings/SettingsPage.vue
+++ frontend/src/pages/Settings/SettingsPage.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="mx-4 min-h-screen max-w-screen-xl sm:mx-8 xl:mx-auto">
     <h1 class="border-b py-6 text-4xl font-semibold">Paramètres</h1>
+    <h1 class="border-b py-6 text-4xl font-semibold">Paramètres</h1>
     <div class="grid grid-cols-8 pt-3 sm:grid-cols-10">
       <!-- Navigation Section -->
-      <div class="col-span-2 hidden sm:block">
-        <ul>
-          <li
-            class="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700"
-          >
-            Équipes
-          </li>
-          <li
-            class="mt-5 cursor-pointer border-l-2 border-l-blue-700 px-2 py-2 font-semibold text-blue-700 transition hover:border-l-blue-700 hover:text-blue-700"
-          >
-            Compte
-          </li>
-        </ul>
-      </div>
+      <SettingsNavigationMenu />
 
       <!-- Content Section -->
       <div
@@ -57,6 +45,7 @@
               class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
               placeholder="Nouveau nom d'utilisateur"
             />
+            />
           </div>
           <button
             class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white"
@@ -67,8 +56,10 @@
         </div>
 
         <hr class="mt-4 mb-8" />
+        <hr class="mt-4 mb-8" />
 
         <!-- Email -->
+        <p class="py-2 text-xl font-semibold">Adresse e-mail</p>
         <p class="py-2 text-xl font-semibold">Adresse e-mail</p>
         <div
           class="flex flex-col sm:flex-row sm:items-center sm:justify-between"
@@ -98,6 +89,7 @@
               class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
               placeholder="Nouvelle adresse e-mail"
             />
+            />
           </div>
           <button
             class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white"
@@ -106,10 +98,12 @@
             Enregistrer l'e-mail
           </button>
         </div>
+
         <!-- <UploadFileComponent/> -->
         <hr class="mt-4 mb-8" />
         <!-- Delete Account -->
         <div class="mb-10">
+          <p class="py-2 text-xl font-semibold">Supprimer le compte</p>
           <p class="py-2 text-xl font-semibold">Supprimer le compte</p>
           <p
             class="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600"
@@ -147,12 +141,12 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue';
 import { useAuthenticateStore, useUsersStore } from '../../app/store/store';
-import UploadFileComponent from '../../components/UploadFile/UploadFileComponent.vue';
+import SettingsNavigationMenu from './SettingsNavigationMenu.vue';
 
 export default defineComponent({
   name: 'SettingsPage',
   components: {
-    UploadFileComponent,
+    SettingsNavigationMenu,
   },
   setup() {
     const authStore = useAuthenticateStore();
