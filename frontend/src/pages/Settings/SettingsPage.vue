da4a12a262cec491b152a03ef02d10b335ac9839
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2024-10-24 02:25:09 +0200

    Updated settings pages and added a controller for document downloads. Downloads still not functioning.

--- frontend/src/pages/Settings/SettingsPage.vue
+++ frontend/src/pages/Settings/SettingsPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mx-4 min-h-screen max-w-screen-xl sm:mx-8 xl:mx-auto">
     <h1 class="border-b py-6 text-4xl font-semibold">Paramètres</h1>
+    <h1 class="border-b py-6 text-4xl font-semibold">Paramètres</h1>
     <div class="grid grid-cols-8 pt-3 sm:grid-cols-10">
       <!-- Navigation Section -->
       <SettingsNavigationMenu />
@@ -46,6 +47,7 @@
               class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
               placeholder="Nouveau nom d'utilisateur"
             />
+            />
           </div>
           <button
             class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white"
@@ -56,8 +58,10 @@
         </div>
 
         <hr class="mt-4 mb-8" />
+        <hr class="mt-4 mb-8" />
 
         <!-- Email -->
+        <p class="py-2 text-xl font-semibold">Adresse e-mail</p>
         <p class="py-2 text-xl font-semibold">Adresse e-mail</p>
         <div
           class="flex flex-col sm:flex-row sm:items-center sm:justify-between"
@@ -87,6 +91,7 @@
               class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
               placeholder="Nouvelle adresse e-mail"
             />
+            />
           </div>
           <button
             class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white"
@@ -96,9 +101,11 @@
           </button>
         </div>
 
+
         <hr class="mt-4 mb-8" />
         <!-- Delete Account -->
         <div class="mb-10">
+          <p class="py-2 text-xl font-semibold">Supprimer le compte</p>
           <p class="py-2 text-xl font-semibold">Supprimer le compte</p>
           <p
             class="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600"
