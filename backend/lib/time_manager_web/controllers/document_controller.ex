1137049eaf588685ca6535d6ace37c90fa157b26
gaetan pecqueux <gaetan.pecqueux@gmail.com>
2024-10-24 16:03:05 +0200

    fix dowload documents

--- backend/lib/time_manager_web/controllers/document_controller.ex
+++ backend/lib/time_manager_web/controllers/document_controller.ex
@@ -3,15 +3,17 @@ defmodule TimeManagerWeb.DocumentController do
   require Logger
 
   def download(conn, %{"filename" => filename}) do
-    file_path = Path.join("documents/", filename)
+
+    file_path = Path.join([:code.priv_dir(:time_manager), "documents", filename])
 
     if File.exists?(file_path) do
-      Logger.info("Fichier trouvé : #{file_path}")
+      Logger.info("Téléchargement du fichier : #{file_path}")
       send_download(conn, {:file, file_path})
     else
       Logger.error("Fichier non trouvé : #{file_path}")
 
       conn
+      |> fetch_flash()
       |> put_flash(:error, "Fichier introuvable")
       |> redirect(to: "/")
     end
