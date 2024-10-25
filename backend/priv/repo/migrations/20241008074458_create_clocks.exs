952361e3b80bbf426918b2038e0cc4574c9dd4c7
JeanCR <jeanclementeradigois@gmail.com>
2024-10-25 17:23:19 +0200

    fix: fixed not null

--- backend/priv/repo/migrations/20241008074458_create_clocks.exs
+++ backend/priv/repo/migrations/20241008074458_create_clocks.exs
@@ -5,7 +5,7 @@ defmodule TimeManager.Repo.Migrations.CreateClocks do
     create table(:clocks) do
       add :time, :naive_datetime
       add :status, :boolean, default: false, null: false
-      add :user, references(:users, on_delete: :nothing), null: false
+      add :user, references(:users, on_delete: :nothing)
 
       timestamps(type: :utc_datetime)
     end
